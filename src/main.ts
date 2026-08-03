7006b7808cb1e83876f4086be9fb02e203f50dd5
saeedyussif663 <saeedyussif663@gmail.com>
2026-08-03 00:31:54 +0000

    railway port issue

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, stopAtFirstError: true }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
